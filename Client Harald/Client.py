7cb16381115438d93f20443a6bd4d142ba2ada83
haralbl <haraldblehr@hotmail.com>
2015-03-06 17:10:47 +0100

    added neccessary functions

--- Client Harald/Client.py
+++ Client Harald/Client.py
@@ -21,104 +21,108 @@ class MessageReceiver(Thread):
 
 class Client:
     def __init__(self, host, server_port):
-        # Set up the socket connection to the server
         self.connection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.host = host
         self.server_port = server_port
         
-
-        # TODO: Finish init process with necessary code
         receiver = MessageReceiver('client', 'connection')
         receiver.setName('receiverThread')
         receiver.start()
-        
-
-        #self.run()
 
     def run(self):
-        # Initiate the connection to the server
-        #self.connection.connect((self.host, self.server_port))
+        self.root = Tk()
+        self.root.title('KTN Chat')
+        self.root.geometry('{}x{}'.format(400, 300))
+        self.send_var = StringVar()
+
+        self.b_login = Button(self.root, text='Log in', command=self.login)
+        self.b_logout = Button(self.root, text='Log out', command=self.logout)
+        self.b_names = Button(self.root, text='List names', command=self.list_names)
+        self.b_chat = Button(self.root, text='Open chat', command=self.open_chat)
+        self.b_help = Button(self.root, text='Help', command=self.help)
+        self.b_quit = Button(self.root, text='Quit', command=self.quit)
+
+        self.w_text_frame = Frame(self.root, height=280, width=400, bg='Black')
+        self.w_text_frame.pack_propagate(False)
+        self.w_write_frame = Frame(self.root, height=20, width=400)
+        self.w_write_frame.pack_propagate(False)
+        self.w_text_scrollbar = Scrollbar(self.w_text_frame)
+
+        self.w_text = Text(self.w_text_frame, font=('Helvetica', 10), bg='Black', fg='Green') #, textvariable=self.recv_var
+        self.w_text.config(yscrollcommand=self.w_text_scrollbar.set)
+        self.w_text_scrollbar.config(command=self.w_text.yview)
+        self.w_text.insert(END, 'Welcome to KTN Chat\nType \quit to exit\n')
+
+        self.w_write = Entry(self.w_write_frame, textvariable=self.send_var, width=400)
+        self.w_write.bind('<Return>', self.send_payload)
 
         self.open_menu()
 
-    def disconnect(self):
-        # TODO: Handle disconnection
-        #self.connection.close()
+        self.root.mainloop()
+
+    def open_menu(self):
+        self.w_text_frame.pack_forget()
+        self.w_write_frame.pack_forget()
+        self.w_text_scrollbar.pack_forget()
+        
+        self.w_text.pack_forget()
+        self.w_write.pack_forget()
+
+        self.b_login.pack()
+        self.b_logout.pack()
+        self.b_names.pack()
+        self.b_chat.pack()
+        self.b_help.pack()
+        self.b_quit.pack()
+
+    def login():
+        pass
+    def logout():
+        pass
+    def list_names():
         pass
 
+    def open_chat(self):
+        self.b_login.pack_forget()
+        self.b_logout.pack_forget()
+        self.b_names.pack_forget()
+        self.b_chat.pack_forget()
+        self.b_help.pack_forget()
+        self.b_quit.pack_forget()
+
+        self.w_text_frame.pack(side=TOP)
+        self.w_write_frame.pack(side=BOTTOM)
+        self.w_text_scrollbar.pack(side=RIGHT, fill=Y)
+        
+        self.w_text.pack()
+        self.w_write.pack()
+
+    def help():
+        pass
+    def quit(self):
+        sys.exit()
+
     def receive_message(self, message):
-        #self.recv_var.set(self.recv_var.get() + message + '\n')
         try:
             self.w_text.insert(END, message + '\n')
             self.w_text.yview(END)
         except:
             pass
-        
 
     def send_payload(self, event):
-        
         send_var = self.w_write.get()
         if send_var == '\quit':
-            self.t_msg.quit()
             self.open_menu()
+        else:
+            print send_var #for debugging
+            # use json on data
+            #self.connection.send(data)
         self.w_write.delete(0, END)
-        print send_var
-        # use json on data
-        #self.connection.send(data)
 
-    def open_menu(self):
-        self.t_menu = Tk()
-        self.t_menu.title('KTN Chat')
-        self.t_menu.geometry('{}x{}'.format(400, 300))
-
-        self.b_login = Button(self.t_menu, text='Log in', command=self.open_chat)
-        self.b_login.pack()
-        self.b_logout = Button(self.t_menu, text='Log out', command=self.open_chat)
-        self.b_logout.pack()
-        self.b_names = Button(self.t_menu, text='List names', command=self.open_chat)
-        self.b_names.pack()
-        self.b_chat = Button(self.t_menu, text='Open chat', command=self.open_chat)
-        self.b_chat.pack()
-        self.b_help = Button(self.t_menu, text='Help', command=self.open_chat)
-        self.b_help.pack()
-        self.b_quit = Button(self.t_menu, text='Quit', command=self.quit)
-        self.b_quit.pack()
-
-        self.t_menu.mainloop()
-
-    def open_chat(self):
-        self.t_menu.quit()
-        
-        self.t_msg = Tk()
-        self.t_msg.title('KTN Chat')
-        self.t_msg.geometry('{}x{}'.format(400, 300))
-        self.send_var = StringVar()
-
-        self.w_text_frame = Frame(self.t_msg, height=280, width=400, bg='Black')
-        self.w_text_frame.pack_propagate(False)
-        self.w_text_frame.pack(side=TOP)
-        self.w_write_frame = Frame(self.t_msg, height=20, width=400)
-        self.w_write_frame.pack_propagate(False)
-        self.w_write_frame.pack(side=BOTTOM)
-
-        self.w_text_scrollbar = Scrollbar(self.w_text_frame)
-        self.w_text_scrollbar.pack(side=RIGHT, fill=Y)
-        
-        self.w_text = Text(self.w_text_frame, font=('Helvetica', 10), bg='Black', fg='Green') #, textvariable=self.recv_var
-        self.w_text.config(yscrollcommand=self.w_text_scrollbar.set)
-        self.w_text_scrollbar.config(command=self.w_text.yview)
-        self.w_text.insert(END, 'Welcome to KTN Chat\nType \quit to exit\n')
-        self.w_text.pack()
-
-        self.w_write = Entry(self.w_write_frame, textvariable=self.send_var, width=400)
-        self.w_write.bind('<Return>', self.send_payload)
-        self.w_write.pack()
-
-        self.t_msg.mainloop()
-
-    def quit(self):
-        sys.exit()
-
+    def disconnect(self):
+        # TODO: Handle disconnection
+        #self.connection.close()
+        pass
 
 if __name__ == '__main__':
     if len(sys.argv) == 1:
@@ -131,5 +135,3 @@ if __name__ == '__main__':
         print 'Too many input arguments'
         sys.exit()
 
-    #subprocess.popen([sys.executable, 'MessageReceiver.py'], 
-    #           creationflags = subprocess.CREATE_NEW_CONSOLE)
